6bc7e5009f00f2b16606be6cd46306d7d1b576ec
Stone <syang477@wisc.edu>
2020-10-28 22:30:28 -0700

    HAS BUG: When last element is deleted, index OOB error occurs. Other than that, have implemented deletion mechanic with swipe gesture

--- TRACE/EditView.swift
+++ TRACE/EditView.swift
@@ -18,63 +18,101 @@ import SwiftUI
 }*/
 
 struct EditView : View {
-    
-
+    @State var data = ["CS506", "Second item", "Third Item", "Complete iteration 1 we got this", "fix index oob error"]
     @Binding var editMode: Bool
     @State var index = 0
+    
+    @State var amountDragged = CGSize.zero
 
     var body : some View {
-        ZStack {
-            Color(rgb: [0, 0, 0], alpha: 0.3)
-                .ignoresSafeArea(.all)
-            
-            // Vertical structure
-            VStack {
-                Text("Tap the Circle to edit\nSwipe up to delete.")
-                    .font(Font.custom("Comfortaa-Regular", size: 18))
-                    .foregroundColor(Color(rgb: WHITE))
-                    .multilineTextAlignment(.center)
-                    .offset(y: 30)
-                
-                // Events List
-                TabView(selection: self.$index) {
-                        ForEach(0...50, id: \.self) {index in
-                            InfoView(index: self.index)
-                                .padding(.horizontal, 5)
-                                .scaleEffect(self.index == index ? 1.0 : 0.3)
-                                .tag(index)
-                                .offset(y: 10)
+        let drag = DragGesture()
+            .onChanged {
+                self.amountDragged = $0.translation
+            }
+            .onEnded {
+                if $0.translation.height < -100 {
+                    withAnimation {
+                        
+                        if self.data.count > 0 && self.data.count > self.index {
+                            self.data.remove(at: self.index)
                         }
-                }
-                .tabViewStyle(PageTabViewStyle())
-                //.offset(y: -70)
-                .animation(.easeOut)
-                .frame(height: UIScreen.main.bounds.height - 240)
-            
-                
-                // Exit Edit Mode
-                Button(action: {
-                        withAnimation{self.editMode.toggle() }}) {
-                    ZStack {
-                        Circle()
-                            .strokeBorder(Color(rgb: WHITE), lineWidth: 3)
-                            .background(Circle().foregroundColor(Color(rgb: RED)))
-                            .frame(width: UIScreen.main.bounds.width / 4, height: UIScreen.main.bounds.width / 4)
-                        Image("close_icon")
-                            .foregroundColor(Color(rgb: DARK_GREY, alpha: 0.9))
-                            .scaleEffect(1.8)
+                        self.amountDragged = .zero
+                        print(self.data)
+                        print(self.data.count)
+                        print(self.index)
                     }
+                } else {
+                    self.amountDragged = .zero
+                    
                 }
-            } // End of VStack
-        } // End of ZStack
+            }
+        
+        GeometryReader { geo in
+            ZStack {
+                Color(rgb: [0, 0, 0], alpha: 0.3)
+                    .ignoresSafeArea(.all)
+                
+                // Vertical structure
+                VStack {
+                    Text("Tap the Circle to edit\nSwipe up to delete.")
+                        .font(Font.custom("Comfortaa-Regular", size: 18))
+                        .foregroundColor(Color(rgb: WHITE))
+                        .multilineTextAlignment(.center)
+                        .offset(y: 30)
+                    
+                    if self.data.count > 0 {
+                        // Events List
+                        TabView(selection: self.$index) {
+                            ForEach(0..<self.data.count, id: \.self) {index in
+                                InfoView(index: self.index, mockData: self.data[self.index])
+                                    .padding(.horizontal, 5)
+                                    .scaleEffect(self.index == index ? 1.0 : 0.3)
+                                    .tag(index)
+                                    .offset(y: amountDragged.height < 0 ? 10 + amountDragged.height : 10)
+                                    .disabled(amountDragged.height < -100 ? true : false)
+                                    .gesture(drag)
+                                }
+                        }
+                        .tabViewStyle(PageTabViewStyle())
+                        //.offset(y: -70)
+                        .animation(.easeOut)
+                        .frame(height: UIScreen.main.bounds.height - 240)
+                    } else {
+                        Spacer()
+                        Text("No scheduled events yet.\nMake one by using the + button on the homepage!")
+                            .font(Font.custom("Comfortaa-Regular", size: 22))
+                            .foregroundColor(Color(rgb: WHITE))
+                            .multilineTextAlignment(.center)
+                            .frame(width: UIScreen.main.bounds.width / 1.3)
+
+                        Spacer()
+                    }
+                    // Exit Edit Mode
+                    Button(action: {
+                            withAnimation{self.editMode.toggle() }}) {
+                        ZStack {
+                            Circle()
+                                .strokeBorder(Color(rgb: WHITE), lineWidth: 3)
+                                .background(Circle().foregroundColor(Color(rgb: RED)))
+                                .frame(width: UIScreen.main.bounds.width / 4, height: UIScreen.main.bounds.width / 4)
+                            Image("close_icon")
+                                .foregroundColor(Color(rgb: DARK_GREY, alpha: 0.9))
+                                .scaleEffect(1.8)
+                        }
+                    }
+                } // End of VStack
+            } // End of ZStack
+        }
     }
 }
 
 struct InfoView : View {
     let index: Int
+    let mockData: String  // replace with data element from database
     
-    init(index: Int) {
+    init(index: Int, mockData: String) {
         self.index = index
+        self.mockData = mockData
         UIScrollView.appearance().bounces = false
     }
     
@@ -83,20 +121,21 @@ struct InfoView : View {
         VStack {
             // Circle Subject Matter
             
-            Button(action: {print("\(index)")}) {
+            // Button to go to "AddView" (in edit mode)
+            Button(action: {print("\(mockData) INDEX: \(index)")}) {
                 ZStack {
                     Circle()
                         .foregroundColor(index % 2 == 0 ? Color(rgb: ORANGE) : Color(rgb: RED))
                         .frame(width: UIScreen.main.bounds.width / 1.4, height: UIScreen.main.bounds.width / 1.4)
                         .shadow(color: index % 2 == 0 ? Color(rgb: ORANGE) : Color(rgb: RED), radius: 6)
-                    Text("CS 506")
+                    Text("\(mockData)")
                         .font(Font.custom("Comfortaa-Light", size: 40))
                         .padding()
                         .foregroundColor(Color(rgb: DARK_GREY))
                         .frame(width: UIScreen.main.bounds.size.width - 145, height: UIScreen.main.bounds.size.width - 160, alignment: .center)
                         .multilineTextAlignment(.center)
                         .fixedSize()
-                        //.offset(y: 5)
+                        .offset(y: 5)
                 }
                 
             }
