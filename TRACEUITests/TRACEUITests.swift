b2c04b0f3fa90d465ab55439e8ebce2c4b067f99
Tony Schulz <tonytonys123@gmail.com>
2020-11-19 15:44:53 -0600

    UI tests updated

--- TRACEUITests/TRACEUITests.swift
+++ TRACEUITests/TRACEUITests.swift
@@ -8,48 +8,62 @@
 import XCTest
 
 class TRACEUITests: XCTestCase {
-    var app: XCUIApplication!
 
-
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//
-//        // In UI tests it is usually best to stop immediately when a failure occurs.
-//        super.setUp()
-//        continueAfterFailure = false
-//        app.launchArguments.append("--uitesting")
-//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-//    }
-
-//    override func tearDownWithError() throws {
-//
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-
-    func testLoginWithInvalidEmail() throws {
-        // UI tests must launch the application that they test.
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        app = XCUIApplication()
-        app.launch()
-        let app = XCUIApplication()
-        let emailTextField = app.textFields["Email"]
-        emailTextField.tap()
-        emailTextField.tap()
-        app.buttons["Send Sign In Link / Login"].tap()
-        emailTextField.tap()
-        app.alerts["The sign in link could not be sent."].scrollViews.otherElements.buttons["OK"].tap()
-        XCTAssert(true)
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSwitchToLineMode() throws {
+        // UI tests must launch the application that they test.
+//        let app = XCUIApplication()
+//        app.launch()
+        
+        let app = XCUIApplication()
+        app.launch()
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("tonytonys123@gmail.com")
+        app.buttons["Send Sign In Link / Login"].tap()
+        app.buttons["menu_icon, menu_arrow"].tap()
+        app.buttons["Line Mode"].tap()
+        app.buttons["Line Mode"].tap()
+        app.buttons["Line Mode"].tap()
+
+        
+    }
+    
+    func testToggleDarkMode() throws {
+        let app = XCUIApplication()
+        app.launch()
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("tonytonys123@gmail.com")
+        app.buttons["Send Sign In Link / Login"].tap()
+        app.buttons["menu_icon, menu_arrow"].tap()
+        app.buttons["Dark Mode"].tap()
+        app.buttons["Dark Mode"].tap()
+        app.buttons["Dark Mode"].tap()
+
+    }
+    
+    func testToggle24HourFormat() throws {
+        let app = XCUIApplication()
+        app.launch()
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("tonytonys123@gmail.com")
+        app.buttons["Send Sign In Link / Login"].tap()
+        app.buttons["menu_icon, menu_arrow"].tap()
+        app.buttons["24 Hour Format"].tap()
+        app.buttons["24 Hour Format"].tap()
+        app.buttons["24 Hour Format"].tap()
+
+    }
+
+    
 }
